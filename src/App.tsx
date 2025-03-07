fe1f69a9cf3287ffb1a0102acc4ba241769cec00
AlexKovalovSHK <alex@shk-info.de>
2025-03-07 10:10:12 +0100

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Dashboard from "./dashboard/Dashboard"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+import logo from "./logo.svg"
+
+const App = () => {
+  return (
+    <div className="">
+      <Dashboard/>
+    </div>
+  )
+}
+
+export default App
