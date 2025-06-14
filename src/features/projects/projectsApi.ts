f8aa9ca9960eb442794fbc54737359b640ab4fbf
AlexKovalovSHK <alex@shk-info.de>
2025-06-14 13:46:12 +0200

    update

--- src/features/projects/projectsApi.ts
+++ src/features/projects/projectsApi.ts
@@ -1,30 +1,50 @@
 import axios from "axios"
 import { apiUrl } from "../../App"
-import { AntragRespDto, AntragsInList, ProjectRespDto, ProjectsInListPagination } from "./type";
+import {
+  AntragRespDto,
+  AntragsInList,
+  ProjectRespDto,
+  ProjectsInListPagination,
+} from "./type"
 
-
-
-export async function fetchProjectsByUserId(userId: string): Promise<AntragsInList[]> {
-    const res = await axios.get(`${apiUrl}/api/v2/projects/owner/${userId}`, {
-        withCredentials: true, // ✅ Обязательно!
-    });
-    return res.data;
+export async function fetchProjectsByUserId(
+  userId: string,
+): Promise<AntragsInList[]> {
+  const res = await axios.get(`${apiUrl}/api/v2/projects/owner/${userId}`, {
+    withCredentials: true, // ✅ Обязательно!
+  })
+  return res.data
 }
 
-export async function fechAntragById(projectId:string): Promise<AntragRespDto> {
-    const res = await axios.get(`${apiUrl}/api/v2/projects/${projectId}`, {withCredentials: true,})
-    //console.log(res.data);
-    return res.data
+export async function fechAntragById(
+  projectId: string,
+): Promise<AntragRespDto> {
+  const res = await axios.get(`${apiUrl}/api/v2/projects/${projectId}`, {
+    headers: { "content-type": "application/json" },
+    withCredentials: true,
+  })
+  //console.log(res.data);
+  return res.data
 }
 
-export async function fechProjectById(projectId:string): Promise<ProjectRespDto> {
-    const res = await axios.get(`${apiUrl}/api/v3/projects/${projectId}`, {withCredentials: true,})
-    console.log(res.data);
-    return res.data
+export async function fechProjectById(
+  projectId: string,
+): Promise<ProjectRespDto> {
+  const res = await axios.get(`${apiUrl}/api/v3/projects/${projectId}`, {
+    withCredentials: true,
+  })
+  console.log(res.data)
+  return res.data
 }
 
-export async function fechProjectsByType(type:number, page?: number): Promise<ProjectsInListPagination> {
-    const res = await axios.get(`${apiUrl}/api/v3/projects/types/${type}?page=${page}`, {withCredentials: true,})
-    //console.log(res.data);
-    return res.data
+export async function fechProjectsByType(
+  type: number,
+  page?: number,
+): Promise<ProjectsInListPagination> {
+  const res = await axios.get(
+    `${apiUrl}/api/v3/projects/types/${type}?page=${page}`,
+    { withCredentials: true },
+  )
+  //console.log(res.data);
+  return res.data
 }
