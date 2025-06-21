5e64d894f66652bc708c6c21dc8bf78068127d68
AlexKovalovSHK <alex@shk-info.de>
2025-06-21 12:29:39 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import DashboardSHK from "./dashboard/DashboardSHK"
 //export const apiUrl = "http://localhost:5001"
 //export const apiUrlFoo = 'http://217.154.5.134:5001';
 //export const apiUrl = "https://hr.shk.solutions";
-//export const apiUrl = import.meta.env.VITE_API_URL
+//export const apiUrl = import.meta.env.VITE_API_URL;
 export const apiUrl = 'https://api.heizreport.dev'
 
 const App = () => {
